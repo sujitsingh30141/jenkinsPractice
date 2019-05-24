b020f3ddd30bbece953a4ccb6987041d57b567dc
Sujit <Sujit@sys.hsd1.ga.comcast.net>
2019-05-24 15:55:41 -0400

    jenkins self practice

---
+++ JenkinsPractice/src/jenkinsPractice.java
@@ -0,0 +1,14 @@
+
+public class jenkinsPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	for (int i=1;i<=10;i++)
+	{
+		System.out.println(i +"   Hello World, This is Sujit ");
+	}
+		
+	}
+
+}
